26755133a03f49429aaa2c2cb7c5f424bb19eb39
acesnoopyz <aysllan.ferreiraks@gmail.com>
2022-07-27 08:11:52 -0300

    100% done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { request } = require("express");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 const port = 3000;
@@ -62,7 +63,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/:customListName", function(req,res){
-    const customListName = req.params.customListName;
+    const customListName = _.capitalize(req.params.customListName);
 
     List.findOne({name: customListName}, function(err, foundList){
         if (!err){
